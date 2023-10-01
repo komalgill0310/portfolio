157d7402678af67d25b35a01d4b61517a326d1d2
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-01 10:49:19 -0400

    delete usused code from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,6 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
