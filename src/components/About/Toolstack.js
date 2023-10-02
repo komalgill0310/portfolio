0bd98afb3432f0ecfd64cd9fe6770acb6d96c246
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-01 21:56:38 -0400

    update techStack and toolStacksection

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,13 +6,16 @@ import {
   SiSlack,
   SiVercel,
   SiMacos,
+  SiNetlify,
+  SiWindows,
+  SiDiscord,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
@@ -21,10 +24,13 @@ function Toolstack() {
         <SiPostman />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiDiscord />
       </Col>
     </Row>
   );
