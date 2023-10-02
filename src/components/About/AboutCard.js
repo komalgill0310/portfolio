0dd3f211b5696df205ecb7b9007a9f4ea388f8a6
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-10-01 21:45:51 -0400

    updated about.js section

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,41 +1,39 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { ImPointRight } from "react-icons/im";
+// import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
   return (
     <Card className="quote-card-view">
       <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
-            <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
-
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
-          </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
-        </blockquote>
+        <p style={{ textAlign: "justify" }}>
+          Hi Everyone, I am <span className="purple">Komalpreet Kaur </span>
+          from <span className="purple"> Brampton, ON, Canada.</span>
+          <br />
+          <br />
+          As far back as I can remember, technology has held me in its
+          mesmerizing embrace. The moment I encountered a website or an app, an
+          irresistible curiosity bubbled within me. How did these virtual
+          wonders come to life? This bewitching question became the cornerstone
+          of my fascination, ultimately guiding me toward a lifelong journey
+          into the heart of the digital realm.
+          <br />
+          <br />
+          Once I chose this path, I started devouring information on building
+          web applications. It took me a few days to determine that JavaScript
+          would be the most appropriate solution to my problem. My first step in
+          learning JavaScript fundamentals was to learn the basics of HTML and
+          CSS. I grasped the basics and created my first functional JavaScript
+          application; a Digital Clock. As a result, I was extremely pleased
+          with the outcome. Since then, I have become more interested in
+          learning JavaScript at a higher level and building applications.
+          <br />
+          <br />
+          As I journey onward, I'm often left in awe of how far I've come. In
+          the midst of 2021, I was a novice with a head full of questions about
+          computer programming. Today, I stand before you as the architect of
+          fully functional front-end applications.
+        </p>
       </Card.Body>
     </Card>
   );
